6c3900c26d29f0f22d8988a00fe4e276d74d27c5
Steffan Mouton <moutonsteffan@gmail.com>
2025-03-17 15:52:52 -0500

    Faction card title is now smaller when too long

--- src/components/FactionCard.vue
+++ src/components/FactionCard.vue
@@ -2,7 +2,7 @@
   <v-card class="faction-card tc-card">
     <v-card-title class="d-flex align-center">
       <FactionIcon :faction="faction" :size="32" class="mr-2" />
-      <span class="tc-heading">{{ faction.name }}</span>
+      <span class="tc-heading" :style="titleStyle">{{ faction.name }}</span>
     </v-card-title>
 
     <v-card-text>
@@ -26,10 +26,20 @@
 <script setup lang="ts">
 import type { Faction } from '../models/faction'
 import FactionIcon from './FactionIcon.vue'
+import { computed } from 'vue'
 
-defineProps<{
+const props = defineProps<{
   faction: Faction
 }>()
+
+const titleStyle = computed(() => {
+  const name = props.faction.name
+  if (name.length <= 20) {
+    return { fontSize: '1rem' } // Default size
+  } else {
+    return { fontSize: '0.9rem' }
+  }
+})
 </script>
 
 <style scoped>
